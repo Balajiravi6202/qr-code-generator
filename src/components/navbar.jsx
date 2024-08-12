ef5ab89e612328579fa214509570e58ba0f99b58
Balaji <balajiravi0510@gmail.com>
2024-08-12 09:32:13 +0530

    nav update

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,14 +5,14 @@ function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl mx-auto p-4 flex items-center justify-between">
         <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src="/image/qrlogo.png" className="h-10" alt="QR Code" />
-          <span className="text-1xl font-semibold dark:text-white">QR Code Generator</span>
+          <span className="text-xl font-semibold dark:text-white">QR Code Generator</span>
         </Link>
 
-        <div className="flex items-center md:hidden">
+        <div className="flex md:hidden">
           <button
             onClick={() => setIsOpen(!isOpen)}
             type="button"
@@ -21,7 +21,6 @@ function Navbar() {
             <span className="sr-only">Open main menu</span>
             <svg
               className="w-5 h-5"
-              aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 17 14"
@@ -40,9 +39,9 @@ function Navbar() {
         <div
           className={`${
             isOpen ? "block" : "hidden"
-          } md:flex flex-col md:flex-row md:items-center w-full md:w-auto mt-4 md:mt-0`}
+          } md:flex md:items-center md:w-auto w-full md:w-auto absolute md:static bg-white dark:bg-gray-900 md:bg-transparent top-16 left-0 md:top-auto md:left-auto md:space-x-8 p-4 md:p-0`}
         >
-          <ul className="flex flex-col md:flex-row md:space-x-8 rtl:space-x-reverse">
+          <ul className="flex flex-col md:flex-row md:space-x-8 space-y-4 md:space-y-0">
             <li>
               <Link
                 to="/"
